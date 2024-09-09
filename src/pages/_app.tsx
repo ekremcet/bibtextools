6246af2cc978a2aeedaccfc7844dacacadc9f180
Ekrem Çetinkaya <ekremcetinkaya.dev@gmail.com>
2024-09-09 14:40:58 +0300

    remove bots

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
           id="pianjs"
           data-code="hsKXS6s0dnl9ABqti34whB8AZH8yN1MS"
         ></script>
-        <script
-          src="https://app.suppa.ai/integration/script.js"
-          data-api-key="3a0335f3-c67e-474d-9edc-d2ca722add6e"
-        ></script>
       </Head>
       <Component {...pageProps} />
     </>
